5b91465649b8abc0b07a8920f8c42e7b2c074054
Husam-H-Yahya <husamhisham220@gmail.com>
2026-01-14 13:14:43 +0200

    database&login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
     }
 
     public function login(Request $request) {
-        // 1. التحقق من البيانات (خطوة مهمة جداً)
+
         $request->validate([
             'id_number' => 'required|string',
             'password'  => 'required',
@@ -28,13 +28,11 @@ class AuthController extends Controller
         $id_number = $request->input('id_number');
         $password = $request->input('password');
 
-        // 2. محاولة تسجيل الدخول عبر الـ Business Logic
         if ($this->userLogic->attemptLogin($id_number, $password)) {
-            // 3. التوجيه إلى اسم الـ route المسمى 'dashboard'
-            return redirect()->route('dashboard');
+
+        return redirect()->route('dashboard');
         }
 
-        // 4. في حال الفشل: العودة مع رسالة خطأ والحفاظ على رقم الهوية المدخل
         return back()
             ->withErrors(['error' => 'رقم الهوية أو كلمة المرور غير صحيحة'])
             ->withInput($request->only('id_number'));
--- database/seeders/MainProjectSeeder.php
+++ database/seeders/MainProjectSeeder.php
@@ -15,19 +15,19 @@ class MainProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        // 1. إنشاء الفئات (بما أنك حذفت الـ description)
+        // 1. إنشاء الفئات
         $adminCat = Category::create(['name' => 'مسؤول']);
         $teacherCat = Category::create(['name' => 'محفظ']);
 
         // 2. إنشاء حساب المسؤول (Admin)
         User::create([
             'full_name'     => 'admin ',
-            'id_number'     => '123456789', // هذا ما ستستخدمه في الدخول
-            'password'      => '123456',     // سيتم تشفيره تلقائياً من الموديل
+            'id_number'     => '123456789',
+            'password'      => '123456',
             'date_of_birth' => '1990-01-01',
             'phone_number'  => '0590000000',
             'address'       => 'غزة',
-            'is_admin'      => true,         // هو مسؤول
+            'is_admin'      => true,         //  مسؤول
             'category_id'   => $adminCat->id,
         ]);
 
