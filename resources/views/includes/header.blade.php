033349f9d5558485ee0d537efec9421df87ca0f3
yahyaissam <1yahyayahya9@gmail.com>
2026-01-14 11:20:58 +0200

    image style

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -47,27 +47,29 @@
             </li>
             <li class="nav-item dropdown user-menu">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                    <img src="./assets/img/avatar5.png" class="user-image rounded-circle shadow" alt="User Image" />
+                    <img src="{{ asset('assets/img/avatar5.png') }}" class="user-image rounded-circle shadow"
+                        alt="User Image" />
                     <span class="d-none d-md-inline">اسم المستخدم</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                     <li class="user-header text-bg-primary">
-                        <img src="./assets/img/user2-160x160.jpg" class="rounded-circle shadow" alt="User Image" />
+                        <img src="{{ asset('assets/img/avatar5.png') }}" class="rounded-circle shadow"
+                            alt="User Image" />
                         <p>
-                            Alexander Pierce - Web Developer
-                            <small>Member since Nov. 2023</small>
+                            سعيد أحمد
+                            <small>مسجل منذ 2026</small>
                         </p>
                     </li>
-                    <li class="user-body">
+                    {{-- <li class="user-body">
                         <div class="row">
                             <div class="col-4 text-center"><a href="#">Followers</a></div>
                             <div class="col-4 text-center"><a href="#">Sales</a></div>
                             <div class="col-4 text-center"><a href="#">Friends</a></div>
                         </div>
-                    </li>
+                    </li> --}}
                     <li class="user-footer">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
-                        <a href="#" class="btn btn-default btn-flat float-end">Sign out</a>
+                        <a href="#" class="btn btn-default btn-flat">الملف الشخصي</a>
+                        <a href="#" class="btn btn-default btn-flat float-end">تسجيل الخروج</a>
                     </li>
                 </ul>
             </li>
