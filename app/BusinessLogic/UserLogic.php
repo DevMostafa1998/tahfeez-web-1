bfd294ac35e103316313b455daa3db2a12721206
Husam-H-Yahya <husamhisham220@gmail.com>
2026-01-19 17:04:32 +0200

    الحضور والغياب

--- app/BusinessLogic/UserLogic.php
+++ app/BusinessLogic/UserLogic.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserLogic
 {
+
+     // محاولة تسجيل الدخول
+
     public function attemptLogin($idNumber, $password)
     {
         if (Auth::attempt(['id_number' => $idNumber, 'password' => $password])) {
@@ -17,18 +20,53 @@ class UserLogic
         return false;
     }
 
+
+    // إنشاء مستخدم جديد
+
     public function storeUser($data)
     {
         return User::create([
             'full_name'     => $data['full_name'],
             'id_number'     => $data['id_number'],
-            'password'      => $data['password'],
+            'password'      => Hash::make($data['password']),
             'date_of_birth' => $data['date_of_birth'],
             'phone_number'  => $data['phone_number'],
             'address'       => $data['address'],
             'category_id'   => $data['category_id'],
-            'is_admin'      => $data['is_admin'] ?? false,
+            'is_admin'      => ($data['is_admin'] == 'مسؤول') ? true : false,
             'creation_by'   => Auth::user()->full_name ?? 'System',
         ]);
+    }
+
+
+     // تحديث بيانات مستخدم
+
+    public function updateUser($user, $data)
+    {
+        $updateData = [
+            'full_name'    => $data['full_name'],
+            'id_number'    => $data['id_number'],
+            'phone_number' => $data['phone_number'],
+            'address'      => $data['address'],
+            'is_admin'     => $data['is_admin'],
+            'category_id'  => $data['category_id'],
+            'updated_by'   => Auth::user()->full_name ?? 'System',
+        ];
+
+        if (!empty($data['password'])) {
+            $updateData['password'] = Hash::make($data['password']);
+        }
+
+        return $user->update($updateData);
+    }
+
+
+     // حذف مستخدم (
+
+    public function deleteUser($user)
+    {
+        $user->deleted_by = Auth::user()->full_name ?? 'System';
+        $user->save();
+        return $user->delete();
     }
 }
